cf9343f927160a83f46f9f245c12f73f478a8d44
chipdelmal <chipdelmal@gmail.com>
2019-08-09 14:12:15 -0700

    Update to fix a compilation error.

--- pkg/MoNeT_MGDrivE/landscape.py
+++ pkg/MoNeT_MGDrivE/landscape.py
@@ -11,30 +11,6 @@ AEDES_EXP_PARAMS = [0.01848777, 1.0e-10, math.inf]
 
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Sortings
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-def sortByDistanceToPOI(poi, landscape, distFun=euclideanDistance):
-    '''
-    Returns a numpy array of sorted coordinates according to the distance
-        to an arbitrary point of interest (POI).
-    NOTE: Make sure the landscape & POI are in latlong form! The output is
-        returned in longlat format.
-    '''
-    # Compute distances to POI
-    latlongs = np.asarray(landscape)
-    distances = [distFun(poi, (row[1], row[0])) for row in latlongs]
-    # Get the sorting of the distances and concatenate to longlats
-    sorting = np.asarray(distances).argsort()
-    distArray = np.asarray([distances])
-    longlatDist = np.concatenate((latlongs, distArray.T), axis=1)
-    # Apply the sorting and return the array
-    longlatDistSort = longlatDist[sorting]
-    return longlatDistSort
-
-
-
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Distances
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -58,6 +34,25 @@ def calculateDistanceMatrix(landscape, distFun=euclideanDistance):
         for (j, coordB) in enumerate(landscape):
             distMatrix[i][j] = distFun(coordA, coordB)
     return distMatrix
+
+
+def sortByDistanceToPOI(poi, landscape, distFun=euclideanDistance):
+    '''
+    Returns a numpy array of sorted coordinates according to the distance
+        to an arbitrary point of interest (POI).
+    NOTE: Make sure the landscape & POI are in latlong form! The output is
+        returned in longlat format.
+    '''
+    # Compute distances to POI
+    latlongs = np.asarray(landscape)
+    distances = [distFun(poi, (row[1], row[0])) for row in latlongs]
+    # Get the sorting of the distances and concatenate to longlats
+    sorting = np.asarray(distances).argsort()
+    distArray = np.asarray([distances])
+    longlatDist = np.concatenate((latlongs, distArray.T), axis=1)
+    # Apply the sorting and return the array
+    longlatDistSort = longlatDist[sorting]
+    return longlatDistSort
 
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

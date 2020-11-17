81d9b01ad0613b9e81feb74cb328a4c3fbe43ed5
chipdelmal <chipdelmal@gmail.com>
2020-11-17 13:38:29 -0600

    Adding export traces plots

--- pkg/MoNeT_MGDrivE/plots.py
+++ pkg/MoNeT_MGDrivE/plots.py
@@ -462,3 +462,56 @@ def exportGeneLegend(labels, colors, filename, dpi):
     legend = plt.legend(handles, labels, loc=3, framealpha=1, frameon=False)
     exportLegend(legend, filename=filename, dpi=dpi)
     plt.close('all')
+
+
+
+def exportTracesPlot(tS, nS, STYLE, PATH_IMG, vLines=[0, 0, 0], hLines=[0], wop=0):
+    figArr = plotNodeTraces(tS, STYLE)
+    axTemp = figArr[0].get_axes()[0]
+    axTemp.set_aspect(aspect=STYLE["aspect"])
+    axTemp.set_xlim(STYLE['xRange'][0], STYLE['xRange'][1])
+    axTemp.set_ylim(STYLE['yRange'][0], STYLE['yRange'][1])
+    axTemp.axes.xaxis.set_ticklabels([])
+    axTemp.axes.yaxis.set_ticklabels([])
+    axTemp.axes.xaxis.set_visible(False)
+    axTemp.axes.yaxis.set_visible(False)
+    axTemp.xaxis.set_tick_params(width=1)
+    axTemp.yaxis.set_tick_params(width=1)
+    axTemp.xaxis.set_ticks(np.arange(0, STYLE['xRange'][1], 365))
+    axTemp.yaxis.set_ticks(np.arange(0, STYLE['yRange'][1], STYLE['yRange'][1]/4))
+    axTemp.grid(which='major', axis='y', lw=.5, ls='-', alpha=0.0, color=(0, 0, 0))
+    axTemp.grid(which='major', axis='x', lw=.5, ls='-', alpha=0.0, color=(0, 0, 0))
+
+
+    axTemp.text(
+        0.975, 0.06, int(wop),
+        verticalalignment='top', horizontalalignment='right',
+        transform=axTemp.transAxes,
+        color='#00000055', fontsize=15
+    )
+
+    days = tS['landscapes'][0].shape[0]
+    if (vLines[0] > 0) and (vLines[1] <= days) and (wop > 0) and (vLines[0] < vLines[1]):
+        axTemp.axvspan(vLines[0], vLines[1], alpha=0.2, facecolor='#3687ff', zorder=0)
+        axTemp.axvline(vLines[0], alpha=0.75, ls='-.', lw=.35, color='#3687ff', zorder=0)
+        axTemp.axvline(vLines[1], alpha=0.75, ls='-.', lw=.35, color='#3687ff', zorder=0)
+
+    if (vLines[0] > 0) and (vLines[1] <= days) and (wop > 0) and (vLines[0] > vLines[1]):
+        axTemp.axvspan(vLines[0], vLines[1], alpha=0.2, facecolor='#FF5277', zorder=0)
+        axTemp.axvline(vLines[0], alpha=0.75, ls='-.', lw=.35, color='#FF1A4B', zorder=0)
+        axTemp.axvline(vLines[1], alpha=0.75, ls='-.', lw=.35, color='#FF1A4B', zorder=0)
+
+    axTemp.axhline(
+            hLines, alpha=.25, zorder=10, ls='--', lw=.35, color='#000000'
+        )
+    for vline in vLines[2:]:
+        axTemp.axvline(vline, alpha=.25, zorder=10, ls='--', lw=.35, color='#000000')
+    axTemp.tick_params(color=(0, 0, 0, 0.5))
+    figArr[0].savefig(
+            "{}/{}.png".format(PATH_IMG, nS),
+            dpi=STYLE['dpi'], facecolor=None, edgecolor='w',
+            orientation='portrait', papertype=None, format='png',
+            transparent=True, bbox_inches='tight', pad_inches=0.05
+        )
+    plt.close('all')
+    return True

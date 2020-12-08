a24570a7adde5cf6aab9d62d504c13844ba229b5
chipdelmal <chipdelmal@gmail.com>
2020-12-08 10:25:03 -0600

    Adding color constants (HLT, WLD, ECO, TRS)

--- pkg/MoNeT_MGDrivE/colors.py
+++ pkg/MoNeT_MGDrivE/colors.py
@@ -1,4 +1,7 @@
+
+import matplotlib
 from matplotlib.colors import LinearSegmentedColormap
+
 
 
 def addHexOpacity(colors, alpha='1A'):
@@ -7,6 +10,92 @@ def addHexOpacity(colors, alpha='1A'):
 
 def replaceHexOpacity(colors, alpha='FF'):
     return [i[:-2]+alpha for i in colors]
+
+
+
+def generateAlphaColorMapFromColor(
+    color
+):
+    """
+    Description:
+        * Generates the cmap necessary to do the landscape heatmaps.
+    In:
+        * color: hex color to alpha cmap
+    Out:
+        * cmap: Color map changing in alpha
+    Notes:
+        * NA
+    """
+    alphaMap = LinearSegmentedColormap.from_list(
+        'tempMap',
+        [(0.0, 0.0, 0.0, 0.0), color],
+        gamma=0
+    )
+    return alphaMap
+
+
+def generateAlphaColorMapFromColorArray(
+    colorArray
+):
+    """
+    Description:
+        * Generates a list of cmaps from a colors list.
+    In:
+        * colorArray: Array containing the colors to cmap.
+    Out:
+        * cmapsList: List of cmaps
+    Notes:
+        * NA
+    """
+    elementsNumb = len(colorArray)
+    cmapsList = [None] * elementsNumb
+    for i in range(0, elementsNumb):
+        cmapsList[i] = generateAlphaColorMapFromColor(colorArray[i])
+    return cmapsList
+
+
+
+###############################################################################
+# Colors
+#   Ecology, Health, Trash, Wild
+###############################################################################
+COLEN = ['#2614ed', '#FF006E', '#45d40c', '#8338EC', '#1888e3', '#BC1097', '#FFE93E', '#3b479d']
+COLHN = ['#FF006E', '#8338EC', '#0C4887']
+COLTN = ['#00a2fe', '#8338EC', '#0C4887']
+COLWN = ['#0eeb10', '#8338EC', '#0C4887']
+# Auto-generate colorsets with required alphas --------------------------------
+(COLEN, COLHN, COLTN, COLWN) = [addHexOpacity(i, alpha='1A') for i in (COLEN, COLHN, COLTN, COLWN)]
+(COLEO, COLHO, COLTO, COLWO) = [replaceHexOpacity(i, alpha='FF') for i in (COLEN, COLHN, COLTN, COLWN)]
+(COLEM, COLHM, COLTM, COLWM) = [generateAlphaColorMapFromColorArray(COLWO) for i in (COLEO, COLHO, COLTO, COLWO)]
+
+
+# #############################################################################
+# Color Palette for Heatmaps
+# #############################################################################
+cdict = {
+        'red':  ((0.0, 1.0, 1.0), (0.1, 1.0, 1.0), (0.5, 0.25, 0.25), (1.0, 0.0, 0.0)),
+        'green':    ((0.0, 1.0, 1.0), (0.1, 1.0, 1.0), (0.5, 0.3, 0.3), (1.0, 0.0, 0.0)),
+        'blue':     ((0.0, 1.0, 1.0), (0.1, 1.0, 1.0), (0.5, 1.0, 1.0), (1.0, 0.25, 0.25))
+    }
+cmapB = matplotlib.colors.LinearSegmentedColormap('cmapK', cdict, 256)
+cdict = {
+        'red':      ((0.0, 1.0, 1.0), (0.1, 1.0, 1.0), (1.0, 0.95, 0.95)),
+        'green':    ((0.0, 1.0, 1.0), (0.1, 1.0, 1.0), (1.0, 0, 0)),
+        'blue':     ((0.0, 1.0, 1.0), (0.1, 1.0, 1.0), (1.0, 0.4, 0.4))
+    }
+cmapC = matplotlib.colors.LinearSegmentedColormap('cmapK', cdict, 256)
+cdict = {
+        'red':      ((0.0, 1.0, 1.0), (0.1, 1.0, 1.0), (1.0, 0, 0)),
+        'green':    ((0.0, 1.0, 1.0), (0.1, 1.0, 1.0), (1.0, 0.65, 0.65)),
+        'blue':     ((0.0, 1.0, 1.0), (0.1, 1.0, 1.0), (1.0, .95, .95))
+    }
+cmapM = matplotlib.colors.LinearSegmentedColormap('cmapK', cdict, 256)
+cdict = {
+        'red':      ((0.0, 1.0, 1.0), (0.1, 1.0, 1.0), (1.0, 0.05, 0.05)),
+        'green':    ((0.0, 1.0, 1.0), (0.1, 1.0, 1.0), (1.0, 0.91, 0.91)),
+        'blue':     ((0.0, 1.0, 1.0), (0.1, 1.0, 1.0), (1.0, 0.06, 0.06))
+    }
+cmapW = matplotlib.colors.LinearSegmentedColormap('cmapK', cdict, 256)
 
 
 # Define 5 colormaps ranging from transparent to opaque.
@@ -57,42 +146,5 @@ rgba_colors = [
 cmaps = [light_blue1, red1, purple1, pink1, dark_blue1]
 
 
-def generateAlphaColorMapFromColor(
-    color
-):
-    """
-    Description:
-        * Generates the cmap necessary to do the landscape heatmaps.
-    In:
-        * color: hex color to alpha cmap
-    Out:
-        * cmap: Color map changing in alpha
-    Notes:
-        * NA
-    """
-    alphaMap = LinearSegmentedColormap.from_list(
-        'tempMap',
-        [(0.0, 0.0, 0.0, 0.0), color],
-        gamma=0
-    )
-    return alphaMap
 
 
-def generateAlphaColorMapFromColorArray(
-    colorArray
-):
-    """
-    Description:
-        * Generates a list of cmaps from a colors list.
-    In:
-        * colorArray: Array containing the colors to cmap.
-    Out:
-        * cmapsList: List of cmaps
-    Notes:
-        * NA
-    """
-    elementsNumb = len(colorArray)
-    cmapsList = [None] * elementsNumb
-    for i in range(0, elementsNumb):
-        cmapsList[i] = generateAlphaColorMapFromColor(colorArray[i])
-    return cmapsList

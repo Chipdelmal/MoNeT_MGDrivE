b7588abe000ec0521438b8fb9e807ba8b9caae8f
chipdelmal <chipdelmal@gmail.com>
2020-12-23 10:12:44 -0600

    Uploading makefile and version upgrade

--- pkg/setup.py
+++ pkg/setup.py
@@ -1,11 +1,20 @@
+import os
 import setuptools
+from version import version as this_version
+
+
+this_directory =  os.path.abspath(os.path.dirname(__file__))
+version_path = os.path.join(this_directory, 'MoNeT_MGDrivE', '_version.py')
+print(this_version)
+with open(version_path, 'wt') as fversion:
+    fversion.write('__version__ = "'+this_version+'"')
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='MoNeT_MGDrivE',
-    version='0.6.1.8',
+    version=this_version,
     url='https://github.com/Chipdelmal/MoNeT_MGDrivE',
     author='Hector M. Sanchez C.',
     author_email='sanchez.hmsc@berkeley.edu',
---
+++ pkg/version.py
@@ -0,0 +1,5 @@
+
+version = '0.7.0.0'
+
+if __name__ == '__main__':
+    print(version)

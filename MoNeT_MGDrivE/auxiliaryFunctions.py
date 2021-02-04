f92647d448d02906dbd0a5cb901d055708c51079
chipdelmal <chipdelmal@gmail.com>
2021-02-04 13:28:52 -0800

    Adding fun to detect if it's an interactive session

--- MoNeT_MGDrivE/auxiliaryFunctions.py
+++ MoNeT_MGDrivE/auxiliaryFunctions.py
@@ -417,3 +417,17 @@ def lenAndDigits(coltn):
     clen = len(coltn)
     digs = len(str(clen))
     return (clen, digs)
+
+
+
+def isNotebook():
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter

0115d10b823fbc4bee070680517a7a564e47a8f0
chipdelmal <chipdelmal@gmail.com>
2021-04-01 14:13:33 -0700

    Addind traces video exporter

--- MoNeT_MGDrivE/plots.py
+++ MoNeT_MGDrivE/plots.py
@@ -537,5 +537,43 @@ def exportTracesPlot(
     return True
 
 
+def exportTracesPlotVideo(
+    tS, nS, STYLE, PATH_IMG, vLines=[0, 0]
+):
+    figArr = plotNodeTraces(tS, STYLE)
+    axTemp = figArr[0].get_axes()[0]
+    axTemp.set_aspect(aspect=STYLE["aspect"])
+    axTemp.set_xlim(STYLE['xRange'][0], STYLE['xRange'][1])
+    axTemp.set_ylim(STYLE['yRange'][0], STYLE['yRange'][1])
+    axTemp.axes.xaxis.set_ticklabels([])
+    axTemp.axes.yaxis.set_ticklabels([])
+    axTemp.axes.xaxis.set_visible(False)
+    axTemp.axes.yaxis.set_visible(False)
+    axTemp.xaxis.set_tick_params(width=2)
+    axTemp.yaxis.set_tick_params(width=2)
+    axTemp.set_axis_off()
+    axTemp.xaxis.set_ticks(np.arange(0, STYLE['xRange'][1], 365))
+    axTemp.yaxis.set_ticks(np.arange(0, STYLE['yRange'][1], STYLE['yRange'][1]/4))
+    axTemp.grid(which='major', axis='y', lw=.5, ls='-', alpha=0.0, color=(0, 0, 0))
+    axTemp.grid(which='major', axis='x', lw=.5, ls='-', alpha=0.0, color=(0, 0, 0))
+
+    # for spine in axTemp.spines.values():
+    #     spine.set_edgecolor('#B1C0DD77')
+
+    days = tS['landscapes'][0].shape[0]
+    axTemp.axvspan(vLines[0], vLines[1], alpha=.5, facecolor='#ffffff', zorder=5)
+    axTemp.axvline(vLines[0], alpha=0.25, ls='--', lw=1, color='#B1C0DD', zorder=10)
+
+    axTemp.tick_params(color=(0, 0, 0, 0.5))
+    figArr[0].savefig(
+            "{}/{}.png".format(PATH_IMG, nS),
+            dpi=STYLE['dpi'], facecolor='w', edgecolor='w',
+            orientation='portrait', format='png',
+            transparent=False, bbox_inches='tight', pad_inches=0
+        )
+    plt.close('all')
+    return True
+
+
 def getAxisRange(x):
     return (min(x), max(x))

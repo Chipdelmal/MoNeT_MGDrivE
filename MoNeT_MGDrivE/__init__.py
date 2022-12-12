7e0bf4f5f6b9d7b797a86b3e15785a505d7bfa0c
chipdelmal <chipdelmal@gmail.com>
2022-12-12 16:07:50 -0600

    Adding ML file for extensions

--- MoNeT_MGDrivE/__init__.py
+++ MoNeT_MGDrivE/__init__.py
@@ -19,3 +19,4 @@ from .dataAnalysis import *
 from .genes import *
 from .parallel import *
 from .mailAlert import *
+from mlModels import *
--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 
-version = '0.9.0.0'
+version = '0.9.0.1'
 
 if __name__ == '__main__':
     print(version)
